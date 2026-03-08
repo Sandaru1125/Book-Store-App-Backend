f0da1471640eaa86e6d8f9b3b962122003241ed4
Sandaru1125 <sandarunirmal55@gmail.com>
2026-03-08 11:21:45 +0530

    inizialyze project

---
+++ src/lib/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export const constDBConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);  
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }        
+}
