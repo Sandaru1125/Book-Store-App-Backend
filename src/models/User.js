be968cb6b5badad53ae0df2d49cbb35b3105c9f5
Sandaru1125 <sandarunirmal55@gmail.com>
2026-03-31 00:01:12 +0530

    Implement password hashing and comparison methods in User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,57 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre-save hook to hash password (async/await style)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+/*
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema({
     username: {
          type: String,
@@ -55,3 +107,4 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
 
 const User = mongoose.model('User', userSchema);
 export default User;
+*/
