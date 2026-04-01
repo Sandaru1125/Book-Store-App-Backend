244497a2334a52524e8f02b18f288211297bbfe8
Sandaru1125 <sandarunirmal55@gmail.com>
2026-04-02 00:42:46 +0530

    Update Cloudinary configuration and increase JSON request size limit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import bookRoutes from './routes/bookRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({
   origin: "*", // allow all (for development)
   methods: ["GET", "POST", "PUT", "DELETE"],
